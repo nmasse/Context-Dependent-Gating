b85d1ffea13b6dcd662c6e76399c3bbc0d2a8ce3
Nicolas Masse <nicolas.masse@gmail.com>
2018-02-04 11:01:25 -0600

    Major updates

--- parameters.py
+++ parameters.py
@@ -1,6 +1,5 @@
 ### Authors: Nicolas Y. Masse, Gregory D. Grant
 
-
 import numpy as np
 import tensorflow as tf
 from itertools import product
@@ -19,43 +18,38 @@ par = {
     'loss_function'         : 'cross_entropy',    # cross_entropy or MSE
     'stabilization'         : 'pathint', # 'EWC' (Kirkpatrick method) or 'pathint' (Zenke method)
     'learning_rate'         : 0.001,
-    'task'                  : 'cifar',
+    'task'                  : 'mnist',
     'save_analysis'         : True,
     'train_convolutional_layers' : False,
 
     # Task specs
     'n_tasks'               : 100,
 
-
-    # Network shape
-    'n_td'                  : 100,
-    'n_dendrites'           : 1,
-    #'layer_dims'            : [28**2, 100, 100, 10], # mnist
-    'layer_dims'            : [4096, 500, 500, 100], #cifar
-    'dendrites_final_layer' : False,
+    'layer_dims'            : [28**2, 2000, 2000, 10], # mnist
+    #'layer_dims'            : [4096, 1000, 1000, 5], #cifar
     'pct_active_neurons'    : 1.0,
     'multihead'             : False, # option for CIFAR task, do we use different output neurons for each label nad add a mask, or recycle them
 
     # Dropout
     'drop_keep_pct'         : 0.5,
-    'input_drop_keep_pct'   : 0.8,
+    'input_drop_keep_pct'   : 1.0,
+    'conv_drop_keep_pct'    : 0.75,
 
     # Training specs
     'batch_size'            : 256,
     'n_train_batches'       : 3906, # 3906*256 = 20 epochs * 50000
-    'n_batches_top_down'    : 15000,
+    'n_batches_top_down'    : 10000,
 
     # Omega parameters
-    'omega_c'               : 0.05*,
-    'omega_xi'              : 0.1,
-
+    'omega_c'               : 0.1,
+    'omega_xi'              : 0.01,
+    'last_layer_mult'       : 2,
+    'scale_factor'          : 1,
 
     # Projection of top-down activity
     # Only one can be True
     'clamp'                 : None, # can be either 'dendrites', 'neurons', 'partial' or None
-
-    'EWC_fisher_calc_batch' : 128, # batch size when calculating EWC
-    'EWC_fisher_num_batches': 64, # number of batches size when calculating EWC
+    'EWC_fisher_num_batches': 32, # number of batches size when calculating EWC
 
 }
 
@@ -63,69 +57,34 @@ par = {
 ### Dependent parameters ###
 ############################
 
-def gen_td_cases():
-
-    # will create par['n_tasks'] number of tunings, each with exactly n non-zero elements equal to one
-    # the distance between all tuned will be d
-
-    par['td_cases'] = np.zeros((par['n_tasks'], par['n_td']), dtype = np.float32)
-
-    #if par['clamp'] == 'neurons':
-    for n in range(par['n_tasks']):
-        par['td_cases'][n, n%par['n_td']] = 1
-
-
-def gen_td_targets():
+def gen_gating():
 
     m = round(1/par['pct_active_neurons'])
-    print('Clamping: selecting every ', m, ' neuron')
 
-    par['td_targets'] = []
-    par['W_td0'] = []
-    for n in range(par['n_layers']-1):
-        td = np.zeros((par['n_tasks'],par['n_dendrites'], par['layer_dims'][n+1]), dtype = np.float32)
-        Wtd = np.zeros((par['n_td'],par['n_dendrites'], par['layer_dims'][n+1]), dtype = np.float32)
-        for i in range(par['layer_dims'][n+1]):
-
-            if par['clamp'] == 'dendrites':
-                for t in range(0, par['n_tasks'], par['n_dendrites']):
-                    q = np.random.permutation(par['n_dendrites'])
-                    for j, d in enumerate(q):
-                        if t+j<par['n_tasks']:
-                            td[t+j,d,i] = 1
-                            Wtd[t+j,d,i] = 1
-
-            elif par['clamp'] == 'neurons':
-                for t in range(0, par['n_tasks']):
-                    if np.random.rand() < 1/m:
-                        td[t,:,i] = 1
-                        Wtd[t,:,i] = 1
-
-            elif par['clamp'] == 'split':
-                for t in range(0, par['n_tasks']):
+    par['gating'] = []
+    for t in range(par['n_tasks']):
+        gating_task = []
+        for n in range(par['n_layers']-2):
+            gating_layer = np.zeros((par['layer_dims'][n+1]), dtype = np.float32)
+            for i in range(par['layer_dims'][n+1]):
+                if par['clamp'] == 'neurons':
+                    if np.random.rand() < par['pct_active_neurons']:
+                        gating_layer[i] = 1
+                elif par['clamp'] == 'split':
                     if t%m == i%m:
-                        if np.random.rand(1) < 0.5:
-                            td[t,:,i] = 0.5
-                            Wtd[t,:,i] = 0.5
+                        if np.random.rand() < 0.5:
+                            gating_layer[i] = 0.5
                         else:
-                            td[t,:,i] = 1
-                            Wtd[t,:,i] = 1
-
-            elif par['clamp'] == 'partial':
-                for t in range(0, par['n_tasks']):
-                    if np.random.rand(1) < 0.5:
-                        td[t,:,i] = 0.5
-                        Wtd[t,:,i] = 0.5
+                            gating_layer[i] = 1
+                elif par['clamp'] == 'partial':
+                    if np.random.rand() < 0.5:
+                        gating_layer[i] = 0.5
                     else:
-                        td[t,:,i] = 1
-                        Wtd[t,:,i] = 1
-
-            elif par['clamp'] is None:
-                td[:,:,:] = 1
-                Wtd[:,:,:] = 1
-
-        par['td_targets'].append(td)
-        par['W_td0'].append(Wtd)
+                        gating_layer[i] = 1
+                elif par['clamp'] is None:
+                    gating_layer[i] = 1
+            gating_task.append(gating_layer)
+        par['gating'].append(gating_task)
 
 
 def update_dependencies():
@@ -134,8 +93,8 @@ def update_dependencies():
     """
 
     par['n_layers'] = len(par['layer_dims'])
-    gen_td_cases()
-    gen_td_targets()
+    par['max_layer_dim'] = np.max(par['layer_dims'][1:-1])
+    gen_gating()
 
 
 def update_parameters(updates):
