5b5a700e2c44557c7dbcfeea0bc5ea08dd58b080
nmasse <masse@uchicago.edu>
2020-09-24 06:26:20 -0400

    Dynamic weights can now be applied to any layer

--- model.py
+++ model.py
@@ -16,6 +16,10 @@ class Model:
         self.learning_rate  = lr
 
         self.config         = config
+        self.layer_dims     = [self.config['n_input']] + \
+                              self.config['size_feedforward'] + \
+                              [self.config['n_output']]
+        self.n_layers       = len(self.layer_dims) - 1
 
         # Build the Tensorflow graph
         self.rnn_cell_loop()
@@ -49,48 +53,30 @@ class Model:
         for i, (input, target) in enumerate(zip(self.input_data, self.target_data)):
 
             x = input
-            for j, d in enumerate(self.config['size_feedforward']):
 
-                if j < len(self.config['size_feedforward']) - 1:
-                    x = self.dense(x, d, 'feedforward'+str(j))
+            # create the low-rank weights using LSTM activity from the
+            # previous time steps
+            dynamic_W = self.create_low_rank_weights(h)
+
+            for j in range(self.n_layers):
+
+                if dynamic_W[j] is None:
+                    # if no low-rank weights created for this layer,
+                    # then use standard dense layer
+                    x = self.dense(x, self.layer_dims[j+1], 'feedforward'+str(j))
                 else:
-                    x = tf.nn.relu(tf.einsum('bi,bij->bj', x, W_effective) + b)
+                    x = self.dynamic_layer(x, dynamic_W[j], self.layer_dims[j+1],
+                                           'feedforward'+str(j))
 
+                # the activity of the second to last layer will project
+                # to the LSTM
+                if j == self.n_layers - 2:
+                    penultimate_layer = x
 
-                # use the output of the second layer as input into the LSTM
-                if j == 1:
-                    lstm_input = tf.concat((x, 3*action, 3*reward), axis = -1)
-                    h, c = self.recurrent_cell(h, c, lstm_input, self.config['size_lstm'], 'LSTM')
+            lstm_input = tf.concat((penultimate_layer, action, reward), axis = -1)
+            h, c = self.recurrent_cell(h, c, lstm_input, self.config['size_lstm'], 'LSTM')
 
-                # generate the low rank matrix
-                # not sure if this should be done before or after the LSTM loop is run
-                u =  self.dense(h, 2*self.config['size_feedforward'][-2], 'u', activation = tf.identity)
-                v =  self.dense(h, 2*self.config['size_feedforward'][-1], 'v', activation = tf.identity)
-                u  = tf.reshape(u,  (self.config['batch_size'], self.config['size_feedforward'][-2], 2))
-                v  = tf.reshape(v,  (self.config['batch_size'], self.config['size_feedforward'][-1], 2))
-                u  = tf.unstack(u, axis = -1)
-                v  = tf.unstack(v, axis = -1)
-
-                # calculate rank 2 dynamic matrix
-                dynamic_W = u[0][:, :, tf.newaxis] @ v[0][:, tf.newaxis, :] + \
-                    u[1][:, :, tf.newaxis] @ v[1][:, tf.newaxis, :]
-
-
-                # weights to the output layer will consist of standard trainable weights,
-                # multiplied with rank-2 dynamic weights calculated above
-                with tf.variable_scope('penultimate_layer', reuse = tf.AUTO_REUSE):
-
-                    W = tf.get_variable('W', shape = (self.config['size_feedforward'][-1], \
-                        self.config['size_feedforward'][-2]), dtype = tf.float32)
-                    b = tf.get_variable('b', shape = (1, self.config['size_feedforward'][-1]), \
-                        initializer = tf.zeros_initializer(), dtype = tf.float32)
-
-                    W_effective = W * (1 + dynamic_W)
-
-
-            #pol_out = tf.einsum('bi,ij->bj', x, W_pol) + b_pol
-            pol_out        = self.dense(h, self.config['n_output'], scope = 'policy', activation = tf.identity)
-
+            pol_out        = x
             val_out        = self.dense(h, 1, scope = 'value', activation = tf.identity)
             action_index   = tf.multinomial(pol_out, 1)
             action         = tf.one_hot(tf.squeeze(action_index), self.config['n_output'])
@@ -104,6 +90,53 @@ class Model:
             self.action.append(action)
             self.reward.append(reward)
             self.activity.append(h)
+
+
+    def dynamic_layer(self, x, dynamic_W, n_output, scope, bias = True, activation = tf.nn.relu):
+
+        with tf.variable_scope(scope, reuse = tf.AUTO_REUSE):
+
+            W = tf.get_variable('W', shape = [x.shape[-1], n_output], dtype = tf.float32)
+            b = tf.get_variable('b', shape = [1, n_output], initializer = tf.zeros_initializer(), \
+                dtype = tf.float32) if bias else 0.
+
+        W_effective = W * (1 + dynamic_W)
+
+        return activation(tf.einsum('bi,bij->bj', x, W_effective) + b, name = 'output')
+
+
+
+    def create_low_rank_weights(self, h):
+
+        # split the neural activity, and project from each part to
+        # different layers
+        h_split = tf.reshape(h, (self.config['batch_size'], self.n_layers, -1))
+        h_split = tf.unstack(h_split, axis = 1)
+
+        dynamic_W = []
+
+        for i in range(self.n_layers):
+            if self.config['dynamic_weights_rank'][i] > 0:
+                N = self.config['dynamic_weights_rank'][i]
+                u =  self.dense(h_split[i], N*self.layer_dims[i], 'u'+str(i), activation = tf.identity)
+                v =  self.dense(h_split[i], N*self.layer_dims[i+1], 'v'+str(i), activation = tf.identity)
+                u  = tf.reshape(u,  (self.config['batch_size'], self.layer_dims[i], N))
+                v  = tf.reshape(v,  (self.config['batch_size'], self.layer_dims[i+1], N))
+                u  = tf.unstack(u, axis = -1)
+                v  = tf.unstack(v, axis = -1)
+
+                dW = tf.zeros((self.config['batch_size'], self.layer_dims[i], \
+                    self.layer_dims[i+1]), dtype = tf.float32)
+
+                for j in range(N):
+                    dW += u[j][:, :, tf.newaxis] @ v[j][:, tf.newaxis, :]
+                dynamic_W.append(dW)
+
+            else:
+                dynamic_W.append(None)
+
+        return dynamic_W
+
 
 
     def recurrent_cell(self, h, c, rnn_input, lstm_size, scope_prefix):
